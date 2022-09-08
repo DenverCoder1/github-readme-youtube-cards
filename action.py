c0d6161bdcdcd410fc3a919034217b1d8ed4771e
Jonah Lawrence <jonah@freshidea.com>
2022-09-08 05:41:59 -0600

    fix typo

--- action.py
+++ action.py
@@ -37,7 +37,7 @@ class VideoParser:
             "title_color": self._title_color,
             "stats_color": self._stats_color,
         }
-        md = f'[![{params["title"]}]({self._base_url}?{urllib.parse.urlencode(params)} "{params["title"]}")]({params["link"]})'
+        md = f'[![{params["title"]}]({self._base_url}?{urllib.parse.urlencode(params)} "{params["title"]}")]({video["link"]})'
         return md.replace("/", "\\/")
 
     def parse_videos(self) -> str:
