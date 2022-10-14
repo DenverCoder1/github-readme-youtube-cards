980f4cda397ff8d6c456b4544e57b4604805d9a2
Jonah Lawrence <jonah@freshidea.com>
2022-10-13 18:56:06 -0600

    refactor: Use pytest fixtures for mock request (#45)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,28 @@
 import pytest
-
 from api import app
+from flask.wrappers import Request
 
 
 @pytest.fixture()
 def client():
+    """A test client for the app"""
     return app.test_client()
+
+
+class MockRequest(Request):
+    """Mock request object for testing"""
+
+    def __init__(self, **kwargs):
+        self.args = kwargs  # type: ignore
+
+    def set_args(self, **kwargs):
+        self.args = kwargs  # type: ignore
+
+    def update_args(self, **kwargs):
+        self.args.update(kwargs)
+
+
+@pytest.fixture()
+def req():
+    """Mock request object for testing with no arguments"""
+    return MockRequest()
